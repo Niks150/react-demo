3687d314c7de3fd47333cf4d9324dd82c33038dc
Niks150 <sandeepswami344@gmail.com>
2025-08-14 11:12:08 +0530

    Tailwind acceptance

---
+++ vite1/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+    
+    <h1 
+    style={{ backgroundColor: 'yellow' }}>
+      hello world</h1>
+    <h2>This is my new project </h2>
+    </>
+  );
+}
+
+export default App
