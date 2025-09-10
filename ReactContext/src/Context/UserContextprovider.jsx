febade2a60d4e26dc1be8043234ca8dc790fc286
Niks150 <sandeepswami344@gmail.com>
2025-09-10 12:23:02 +0530

    user context

--- ReactContext/src/Context/UserContextprovider.jsx
+++ ReactContext/src/Context/UserContextprovider.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import UserContext from "./UserContext";
 
-const UserContextprovider = ({children}) => {
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
     return(
-        <>
+        <UserContext.Provider value={{user, setUser}}>
         {children}
-        </>
+        </UserContext.Provider>
     )
-
 }
 
-export default UserContextprovider;
+export default UserContextProvider
